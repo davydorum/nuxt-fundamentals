f626182f5c03de385dfb70da39eb03c2a2cb4e6f
David Jiménez Mirayo <david.jimenez.mirayo@gmail.com>
2019-03-22 07:25:45 +0100

    commit inicial

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <the-navbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar'
+
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  all: [
+    {
+      id: 'balut',
+      title: 'What is Balut?',
+      content: 'Content of the first post'
+    },
+    {
+      id: 'wherIsIt',
+      title: 'Where is the sign in button?',
+      content: 'Content of the second post'
+    },
+    {
+      id: 'how',
+      title: 'How can I fix this problem?',
+      content: 'Content of the third post'
+    }
+  ]
+})
